0037a129e81553863c89785776ebf857b123d046
Mariami Tetradze <mariamtetradze9@gmail.com>
2021-07-27 16:01:03 +0400

    add groups and retry analyzer

--- src/test/java/RadioButtonTests.java
+++ src/test/java/RadioButtonTests.java
@@ -15,24 +15,29 @@ public class RadioButtonTests extends ConfigTests{
     SoftAssert soft = new SoftAssert();
 
     public RadioButtonTests(){
-        baseUrl= "https://demoqa.com/radio-button ";
+       // baseUrl= "https://demoqa.com/radio-button ";
         reportsFolder="src/main/resources/RadioButtonFailedTests";
     }
+    /*
     @BeforeTest
     public void beforeRadioButtonTests() {
         open(baseUrl);
-    }
+    } */
 
-    @Test(description = "select yes option", priority = 2)
+    @Test(description = "select yes option", priority = 2,groups = {"FrontEnd"})
     public void selectYes() {
+        baseUrl= "https://demoqa.com/radio-button ";
+        open(baseUrl);
         SelenideElement yesRadio = $(withText("Yes"));;
         yesRadio.click();
         soft.assertFalse(yesRadio.isSelected());
         soft.assertAll();
     }
 
-    @Test(description = "check if no option is available",priority = 1)
+    @Test(description = "check if no option is available",priority = 1, groups = {"BackEnd"})
     public void checkIfDisabled() {
+        baseUrl= "https://demoqa.com/radio-button ";
+        open(baseUrl);
         SelenideElement noRadio = $(withText("No"));
         soft.assertFalse(noRadio.isEnabled());
         soft.assertAll();
